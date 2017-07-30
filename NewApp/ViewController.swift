b5911d6e0986e089fa51d763bb45163386043147
marti168 <marti168@unlv.nevada.edu>
2017-07-30 01:09:31 -0700

    Initial Commit

---
+++ NewApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NewApp
+//
+//  Created by Alberto Martinez on 7/30/17.
+//  Copyright © 2017 Alberto Martinez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
